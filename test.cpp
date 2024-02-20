12f2623040931553f5cec6a88bfa9e23c80e4581
kharada2 <kota.harada@woven-planet.global>
2024-02-21 08:28:41 +0900

    response to review

--- test.cpp
+++ test.cpp
@@ -1,34 +1,36 @@
+#include <float.h>
+
 #include <cassert>
 #include <iostream>
 
 #include "math_operations.h"
 
 void test_addition() {
-  assert(MathOps::addition(2.0, 4.0) == 6.0);
-  assert(MathOps::addition(10.0, 20.0) == 30.0);
+  assert(std::abs(MathOps::addition(2.0, 4.0) - 6.0) <= FLT_EPSILON);
+  assert(std::abs(MathOps::addition(10.0, 20.0) - 30.0) <= FLT_EPSILON);
 
   return;
 }
 
 void test_subtraction() {
-  assert(MathOps::subtraction(2.0, 1.0) == 1.0);
-  assert(MathOps::subtraction(10.0, 20.0) == -10.0);
+  assert(std::abs(MathOps::subtraction(2.0, 1.0) - 1.0) <= FLT_EPSILON);
+  assert(std::abs(MathOps::subtraction(10.0, 20.0) - (-10.0)) <= FLT_EPSILON);
 
   return;
 }
 
 void test_multiplication() {
-  assert(MathOps::multiplication(2.0, 1.0) == 2.0);
-  assert(MathOps::multiplication(10.0, 20.0) == 200.0);
+  assert(std::abs(MathOps::multiplication(2.0, 1.0) - 2.0) <= FLT_EPSILON);
+  assert(std::abs(MathOps::multiplication(10.0, 20.0) - 200.0) <= FLT_EPSILON);
 
   return;
 }
 
 void test_division() {
   // 通常の除算のテスト
-  assert(MathOps::division(2.0, 1.0) == 2.0);
-  assert(MathOps::division(10.0, 5.0) == 2.0);
-  assert(MathOps::division(3.0, 2.0) == 1.5);
+  assert(std::abs(MathOps::division(2.0, 1.0) - 2.0) <= FLT_EPSILON);
+  assert(std::abs(MathOps::division(10.0, 5.0) - 2.0) <= FLT_EPSILON);
+  assert(std::abs(MathOps::division(3.0, 2.0) - 1.5) <= FLT_EPSILON);
 
   // エラーメッセージの確認
   try {
