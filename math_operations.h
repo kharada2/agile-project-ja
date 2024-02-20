b6cc313fdfd026d2a51e37b2587d96d301b58764
Hiroshi Yamada <hiroshi.yamada426@gmail.com>
2024-02-20 23:23:33 +0900

    Add continuous calculation mode

--- math_operations.h
+++ math_operations.h
@@ -5,4 +5,5 @@ float multiplication(float num1, float num2);
 float division(float num1, float num2);
 float sqrtroot(float num1);
 float exponentiation(float num1, float num2);
+float execute_calculation(float num1, char symbol, float num2);
 }  // namespace MathOps
